15050bc5af1771d66c467bdf48553ab430af7c34
unknown <naazfalak5119@gmail.com>
2023-10-22 14:52:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Word_counter from "./Components/word_counter";
+import "../src/Components/styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Word_counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/word_counter.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Word_counter() {
+  const [state, setState] = React.useState({
+    wordCount: 0,
+    charCount: 0,
+  });
+
+  const handleKeyPress = (e) => {
+    const count = e.target.value;
+
+    const countWords = (count) => {
+      if (count.length === 0) {
+        return 0;
+      } else {
+        count = count.replace(/(^\s*)|(\s*$)/gi, "");
+        count = count.replace(/[ ]{2,}/gi, " ");
+        count = count.replace(/\n /, "\n");
+        return count.split(" ").length;
+      }
+    };
+
+    setState({
+      wordCount: countWords(count),
+      charCount: count.length,
+    });
+  };
+
+  return (
+    <div id="container">
+      <h1>
+        Word Count:
+        <span className="num"> {state.wordCount}</span>
+      </h1>
+      <textarea placeholder="Type here" onChange={handleKeyPress}></textarea>
+    </div>
+  );
+}
+
+export default Word_counter;
